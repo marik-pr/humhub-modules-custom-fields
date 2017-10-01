97053110f0e52af32f731a670314515cd8db028b
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-01 13:23:40 +0900

    Update index.php

--- views/edit/index.php
+++ views/edit/index.php
@@ -1,16 +1,11 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 ?>
 
 <div class="panel panel-default">
 
-    <div class="panel-heading"><?php echo Yii::t('HhcfModule.Base', '<strong>HHCF</strong>  module configuration'); ?></div>
-    <div class="control-label">&nbsp;&nbsp;<?php echo Yii::t('HhcfModule.Space', '<strong>Edit template</strong>'); ?>:</div>
+    <div class="panel-heading"><?= Yii::t('HhcfModule.Base', '<strong>HHCF</strong>  module configuration'); ?></div>
+    <div class="control-label">&nbsp;&nbsp;<?= Yii::t('HhcfModule.Space', '<strong>Edit template</strong>'); ?>:</div>
     <div class="panel-body">
 
     </div>
