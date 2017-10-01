836a30ff44e3cac8c164eb63d8cce131aee0cd08
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-10-01 13:25:47 +0900

    Update index.php

--- views/space/index.php
+++ views/space/index.php
@@ -1,17 +1,16 @@
 <?php
 
-
-\humhub\modules\hhcf\assets\Assets::register($this);
 use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
 use humhub\modules\space\modules\manage\widgets\DefaultMenu;
 
+\humhub\modules\hhcf\assets\Assets::register($this);
 ?>
 
 <div class="panel panel-default">
     <div>
         <div class="panel-heading">
-            <?php echo Yii::t('SpaceModule.views_settings', '<strong>Space</strong> settings'); ?>
+            <?= Yii::t('SpaceModule.views_settings', '<strong>Space</strong> settings'); ?>
         </div>
     </div>
 
@@ -19,11 +18,11 @@ use humhub\modules\space\modules\manage\widgets\DefaultMenu;
     <div class="panel-body">
  
         <hr>
-        <?php echo Yii::t('HhcfModule.Space', '<strong>Select template</strong>'); ?>
+        <?= Yii::t('HhcfModule.Space', '<strong>Select template</strong>'); ?>
         <hr>
         <div class="template_box">
             <div class="template_select">-</div>
-            <div class="template_add"><a href=""><?php echo Yii::t('HhcfModule.Space', '<strong>Add template</strong>'); ?></a></div>
+            <div class="template_add"><a href=""><?= Yii::t('HhcfModule.Space', '<strong>Add template</strong>'); ?></a></div>
         </div>
 
     </div>
